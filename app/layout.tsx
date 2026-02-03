4124f5ac61f1a64422f9696a78e9a67e52a3bcbe
Gerson Sanchez Diaz <gsanchez@x-pertec.com>
2026-02-02 23:48:31 -0500

    feat: agregar soporte de localización en español y actualizar el layout con botones de inicio de sesión

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import {
 } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { esES } from "@clerk/localizations";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,25 +32,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="es">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ClerkProvider localization={esES}>
           <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
-              <SignInButton />
-              <SignUpButton>
+              <SignInButton>
+                <button className="bg-white text-[#6c47ff] border border-[#6c47ff] rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Iniciar Sesión
+                </button>
+              </SignInButton>
+              {/* <SignUpButton>
                 <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                   Sign Up
                 </button>
-              </SignUpButton>
+              </SignUpButton> */}
             </SignedOut>
             <SignedIn>
               <UserButton />
             </SignedIn>
           </header>
           {children}
-        </body>
-      </html>
-    </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
